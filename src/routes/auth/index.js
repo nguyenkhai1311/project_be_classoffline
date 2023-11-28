19f222f64671cd2b58b8333843ba94c28a52f3c8
nguyenkhai1311 <nguyenhuukhai1303@gmail.com>
2023-11-28 19:37:42 +0700

    Middleware Login

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -2,11 +2,13 @@ var express = require("express");
 var router = express.Router();
 const passport = require("passport");
 
+const GuestMiddleware = require("../../http/middlewares/GuestMiddleware");
+
 const AuthController = require("../../http/controllers/auth/AuthController");
 const facebookRouter = require("./facebook");
 const googleRouter = require("./google");
 
-router.get("/login", AuthController.login);
+router.get("/login", GuestMiddleware, AuthController.login);
 router.post(
     "/login",
     passport.authenticate("local", {
