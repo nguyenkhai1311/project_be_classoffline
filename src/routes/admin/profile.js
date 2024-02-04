b5bb9c627c1445b469d6a8e76ac5343a4f6a75e9
nguyenkhai1311 <nguyenhuukhai1303@gmail.com>
2024-02-05 00:38:27 +0700

    Add Validate Profile Update And Change Password

--- src/routes/admin/profile.js
+++ src/routes/admin/profile.js
@@ -2,9 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 const ProfileController = require("../../http/controllers/admin/ProfileController");
+const ProfileUpdateValidate = require("../../http/middlewares/ProfileUpdateValidate");
 
 router.get("/", ProfileController.profile);
 router.get("/update", ProfileController.edit);
-router.post("/update", ProfileController.update);
+router.post("/update", ProfileUpdateValidate(), ProfileController.update);
 
 module.exports = router;
